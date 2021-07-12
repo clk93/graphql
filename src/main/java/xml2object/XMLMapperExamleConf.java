ca01f9b34dc88049671ec9e79f1087a06860717d
clk93 <tina_lukas@web.de>
2021-07-12 23:24:46 +0200

    fix string to map

--- src/main/java/xml2object/XMLMapperExamleConf.java
+++ src/main/java/xml2object/XMLMapperExamleConf.java
@@ -8,6 +8,7 @@ import com.google.common.io.Resources;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class XMLMapperExamleConf {
 
@@ -37,20 +38,13 @@ public class XMLMapperExamleConf {
 
 
         String value = source.getAttribute().get(1).getValue();
-        List<String> split = Arrays.asList(value.split("\n"));
-        Map<String, String> objectObjectHashMap = new HashMap<>();
-        split.forEach(s -> {
-            String[] split1 = s.split("=");
-            if(split1.length<= 1){
-                objectObjectHashMap.put(split1[0], null);
-            }
-            else{
-                objectObjectHashMap.put(split1[0], split1[1]);
-            }
-        });
+        Map<String, String> collect1 = Arrays.stream(value.split("\\n"))
+                .map(entry -> entry.split("="))
+                .filter(entry -> entry.length ==2)
+                .collect(Collectors.toMap(entry -> entry[0].trim(), entry -> entry[1].trim()));
+        System.out.println(collect1);
+        System.out.println(collect1.get("variante.label"));
 
-        String standardXML = objectObjectHashMap.get("standardXML");
-        System.out.println(standardXML);
     }
 
     private static String getAttributeByKey(List<Attribute> attribute, String key) {
